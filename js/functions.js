86b5c4baa634751a83f8716047d032630b0d88a3
Alexandra0107 <167964471+Alexandra0107@users.noreply.github.com>
2025-11-28 11:05:55 +0400

    Правки ДЗ

--- js/functions.js
+++ js/functions.js
@@ -5,9 +5,14 @@
 если строка меньше или равна указанной длине,
 и false, если строка длиннее.*/
 
-const isStringWithinLimit = (str = '', maxLength = 20) => str.length <= maxLength;
+const isStringWithinLimit = (str, maxLength) => str.length <= maxLength;
 
-isStringWithinLimit();
+// Строка короче 20 символов
+console.log(isStringWithinLimit('проверяемая строка', 20)); // true
+// Длина строки ровно 18 символов
+console.log(isStringWithinLimit('проверяемая строка', 18)); // true
+// Строка длиннее 10 символов
+console.log(isStringWithinLimit('проверяемая строка', 10)); // false
 
 /*Функция для проверки, является ли строка палиндромом.
 Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево*/
@@ -22,7 +27,14 @@ const isPalindrome = (string) => {
   return string === reversed;
 };
 
-isPalindrome();
+// Строка является палиндромом
+console.log(isPalindrome('топот')); // true
+// Несмотря на разный регистр, тоже палиндром
+console.log(isPalindrome('ДовОд')); // true
+// Это не палиндром
+console.log(isPalindrome('Кекс'));  // false
+// Это палиндром
+console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
 
 /*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 Если в строке нет ни одной цифры, функция должна вернуть NaN*/
@@ -45,4 +57,11 @@ const filterDigitsFromString = (str) => {
   return digits ? parseInt(digits,10) : NaN;
 };
 
-filterDigitsFromString();
+console.log(filterDigitsFromString('2023 год'));            // 2023
+console.log(filterDigitsFromString('ECMAScript 2022'));     // 2022
+console.log(filterDigitsFromString('1 кефир, 0.5 батона')); // 105
+console.log(filterDigitsFromString('агент 007'));           // 7
+console.log(filterDigitsFromString('а я томат'));           // NaN
+console.log(filterDigitsFromString(2023));                 // 2023
+console.log(filterDigitsFromString(-1));                   // 1
+console.log(filterDigitsFromString(1.5));                  // 15
