e595cbdda9d5614c486d9196463893b78e9a787c
Alexandra0107 <alexandra.awerjanova@yandex.ru>
2026-01-21 12:20:02 +0300

    Правки

--- js/functions.js
+++ js/functions.js
@@ -8,11 +8,11 @@
 const isStringWithinLimit = (str, maxLength) => str.length <= maxLength;
 
 // Строка короче 20 символов
-console.log(isStringWithinLimit('проверяемая строка', 20)); // eslint-disable-next-line no-console
+console.log(isStringWithinLimit('проверяемая строка', 20));// eslint-disable-next-line no-console
 // Длина строки ровно 18 символов
-console.log(isStringWithinLimit('проверяемая строка', 18)); // eslint-disable-next-line no-console
+console.log(isStringWithinLimit('проверяемая строка', 18));// eslint-disable-next-line no-console
 // Строка длиннее 10 символов
-console.log(isStringWithinLimit('проверяемая строка', 10)); // eslint-disable-next-line no-console
+console.log(isStringWithinLimit('проверяемая строка', 10));// eslint-disable-next-line no-console
 
 /*Функция для проверки, является ли строка палиндромом.
 Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево*/
@@ -23,12 +23,12 @@ const isPalindrome = (string) => {
 };
 
 // Строка является палиндромом
-console.log(isPalindrome('топот')); // eslint-disable-next-line no-console
+console.log(isPalindrome('топот'));// eslint-disable-next-line no-console
 // Несмотря на разный регистр, тоже палиндром
-console.log(isPalindrome('ДовОд')); // eslint-disable-next-line no-console
-console.log(isPalindrome('Кекс')); // eslint-disable-next-line no-console
+console.log(isPalindrome('ДовОд'));// eslint-disable-next-line no-console
+console.log(isPalindrome('Кекс'));// eslint-disable-next-line no-console
 // Это палиндром
-console.log(isPalindrome('Лёша на полке клопа нашёл ')); // eslint-disable-next-line no-console
+console.log(isPalindrome('Лёша на полке клопа нашёл '));// eslint-disable-next-line no-console
 
 /*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 Если в строке нет ни одной цифры, функция должна вернуть NaN*/
