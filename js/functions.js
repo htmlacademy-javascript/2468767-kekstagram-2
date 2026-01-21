395a2c49b8ff369ce38e23c8f080be8045d60a62
Alexandra0107 <alexandra.awerjanova@yandex.ru>
2026-01-21 12:07:13 +0300

    правки

--- js/functions.js
+++ js/functions.js
@@ -8,11 +8,11 @@
 const isStringWithinLimit = (str, maxLength) => str.length <= maxLength;
 
 // Строка короче 20 символов
-console.log(isStringWithinLimit('проверяемая строка', 20)); // true
+console.log(isStringWithinLimit('проверяемая строка', 20)); // eslint-disable-next-line no-console
 // Длина строки ровно 18 символов
-console.log(isStringWithinLimit('проверяемая строка', 18)); // true
+console.log(isStringWithinLimit('проверяемая строка', 18)); // eslint-disable-next-line no-console
 // Строка длиннее 10 символов
-console.log(isStringWithinLimit('проверяемая строка', 10)); // false
+console.log(isStringWithinLimit('проверяемая строка', 10)); // eslint-disable-next-line no-console
 
 /*Функция для проверки, является ли строка палиндромом.
 Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево*/
@@ -23,13 +23,12 @@ const isPalindrome = (string) => {
 };
 
 // Строка является палиндромом
-console.log(isPalindrome('топот')); // true
+console.log(isPalindrome('топот')); // eslint-disable-next-line no-console
 // Несмотря на разный регистр, тоже палиндром
-console.log(isPalindrome('ДовОд')); // true
-// Это не палиндром
-console.log(isPalindrome('Кекс')); // false
+console.log(isPalindrome('ДовОд')); // eslint-disable-next-line no-console
+console.log(isPalindrome('Кекс')); // eslint-disable-next-line no-console
 // Это палиндром
-console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
+console.log(isPalindrome('Лёша на полке клопа нашёл ')); // eslint-disable-next-line no-console
 
 /*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 Если в строке нет ни одной цифры, функция должна вернуть NaN*/
@@ -52,11 +51,11 @@ const filterDigitsFromString = (str) => {
   return digits ? parseInt(digits, 10) : NaN;
 };
 
-console.log(filterDigitsFromString('2023 год'));  // eslint-disable-next-line no-console
-console.log(filterDigitsFromString('ECMAScript 2022'));  // eslint-disable-next-line no-console
-console.log(filterDigitsFromString('1 кефир, 0.5 батона'));  // eslint-disable-next-line no-console
-console.log(filterDigitsFromString('агент 007'));  // eslint-disable-next-line no-console
-console.log(filterDigitsFromString('а я томат'));  // eslint-disable-next-line no-console
-console.log(filterDigitsFromString(2023));  // eslint-disable-next-line no-console
-console.log(filterDigitsFromString(-1));  // eslint-disable-next-line no-console
+console.log(filterDigitsFromString('2023 год')); // eslint-disable-next-line no-console
+console.log(filterDigitsFromString('ECMAScript 2022')); // eslint-disable-next-line no-console
+console.log(filterDigitsFromString('1 кефир, 0.5 батона')); // eslint-disable-next-line no-console
+console.log(filterDigitsFromString('агент 007')); // eslint-disable-next-line no-console
+console.log(filterDigitsFromString('а я томат')); // eslint-disable-next-line no-console
+console.log(filterDigitsFromString(2023)); // eslint-disable-next-line no-console
+console.log(filterDigitsFromString(-1)); // eslint-disable-next-line no-console
 console.log(filterDigitsFromString(1.5)); // eslint-disable-next-line no-console
