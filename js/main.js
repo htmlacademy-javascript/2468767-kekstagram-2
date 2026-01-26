786d97a9ce465b6e87413e7e373dbecc8075e52c
Alexandra0107 <alexandra.awerjanova@yandex.ru>
2026-01-26 08:27:23 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ import './thumbs.js';
 
 const FOTO = [id, url, descriptions, likes, comments];
 //Проходимся по каждому массиву и генерм случайный индекс
-const FOTO_LIST = FOTO.map(subArr => {
+const FOTO_LIST = FOTO.map((subArr) => {
   const randomIndex = Math.floor(Math.random() * subArr.length);
   return subArr[randomIndex];
 });
+
 console.log(FOTO_LIST);
 
