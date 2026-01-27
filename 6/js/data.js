2d08b7599f2f3854dc1b07856463f095b584f0aa
Keks <keks@htmlacademy.ru>
2026-01-27 17:26:00 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,10 @@
+import {generateComments} from './util.js';
+
+const FOTO = [id, url, descriptions, likes, comments];
+//Проходимся по каждому массиву и генерм случайный индекс
+const FOTO_LIST = FOTO.map(subArr => {const randomIndex = Math.floor(Math.random() * subArr.length);
+  return subArr[randomIndex];
+});
+console.log(FOTO_LIST);
+/* eslint-disable no-console */
+export {FOTO_LIST};
