7515a7033f61b700de639add3230aac7a196e202
Alexandra0107 <167964471+Alexandra0107@users.noreply.github.com>
2026-01-29 21:09:19 +0400

    Правки

--- js/thumbs.js
+++ js/thumbs.js
@@ -1,53 +1,54 @@
 import { getRandomInt, getRandomArrayName } from './util.js';
-import { names, messages } from './data.js'
+import { names, messages } from './data.js';
 
-// вычисляем случайный ID
-const id = Array.from({ length: 25 }, (value, i) => i + 1);
-// вычисляем случайный url
-// eslint-disable-next-line no-unused-vars
-const url = Array.from({ length: 25 }, (value, i) => ({ url: 'photos { i + 1 }.jpg' }));
-const likes = getRandomInt(15, 200);
-// Случайное описание
-const descriptions = [];
-for (let i = 1; i <= 25; i++) {
-  // eslint-disable-next-line no-template-curly-in-string
-  descriptions.push({ description: 'Описание фотографии №{i}' });
-}
-
+const ARRAY_LEN = 25;
 // Функция: генерация массива комментариев
 function generateComments() {
   const commentsCount = getRandomInt(0, 30); // 0–30 комментариев
   const comments = [];
   const usedIds = new Set();
-
   for (let i = 0; i < commentsCount; i++) {
-    let Id;
+    let commentsId;
     // Уникальный id
     do {
-      Id = getRandomInt(1, 100000);
-    } while (usedIds.has(Id));
-    usedIds.add(Id);
+      commentsId = getRandomInt(1, 100000);
+    } while (usedIds.has(commentsId));
+    usedIds.add(commentsId);
     // Функция: случайное сообщение (1 или 2 предложения)
-    function getRandomMessage() {
+    const generateComments = () => {
+      // Определяем количество комментариев (1 или 2)
       const count = getRandomInt(1, 2);
       const result = [];
+
+      // Выбираем случайные сообщения из массива
       for (let i = 0; i < count; i++) {
         result.push(messages[getRandomInt(0, messages.length - 1)]);
       }
+
+      // Возвращаем строку с комментариями, разделёнными пробелом
       return result.join(' ');
-    }
+    };
     // Формируем объект комментария: name в конце
     comments.push({
-      Id: Id,
-      avatar: 'img/avatar-{getRandomInt(1, 6)}.svg',
-      message: getRandomMessage(),
+      id: commentsId,
+      avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+      message: generateComments(messages),
       name: getRandomArrayName(names)
     });
   }
 
   return comments;
 }
+// функция, которая сразу возвращает объект по заданию
+const generatePost = (index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+  likes: getRandomInt(15, 200),
+  descriptions: `Описание фотографии №${index + 1}`,
+  comments: generateComments()
+});
 
-const comments = generateComments();
+// Функция генерации в массив из 25 объектов
+const getThumbs = () => Array.from({ length: ARRAY_LEN }, (_, index) => generatePost(index));
 
-export const FOTO = [id, url, descriptions, likes, comments];
+export { getThumbs };
--- js/util.js
+++ js/util.js
@@ -1,13 +1,7 @@
 // Функция: случайное целое число в диапазоне [min, max]
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const getRandomInt = (min, max)=> Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Функция: рандомный выбор случайного индекса из массива
-function getRandomName() {
-  return Math.floor(Math.random() * ' '.lenght);
-}
-
-const getRandomArrayName = () => [getRandomName(0, ' '.length - 1)];
+const getRandomArrayName = (index) => Math.floor(Math.random() * index.lenght);
 
 export {getRandomInt,getRandomArrayName};
